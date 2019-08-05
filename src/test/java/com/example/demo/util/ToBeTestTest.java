e5b3ac50c09b0a8fe630012352d78fb4648c897f
feng <feng@163.com>
2019-08-05 11:52:18 +0800

    修改测试为spring环境，可以正常使用注入@Autowired

--- src/test/java/com/example/demo/util/ToBeTestTest.java
+++ src/test/java/com/example/demo/util/ToBeTestTest.java
@@ -1,31 +1,22 @@
 package com.example.demo.util;
 
-import com.example.demo.DemoApplication;
 import org.apache.velocity.exception.MathException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest
-//@WebAppConfiguration
-@ComponentScan(basePackages = "com.example.demo.util")
-@ContextConfiguration({"classpath:application.properties"})
+@SpringBootTest
 public class ToBeTestTest {
 
     @Autowired
-    IToBeTest toBeTest2;
+    IToBeTest toBeTest;
 
     @Test
     public void add() {
-        IToBeTest toBeTest = new ToBeTest();
         Assert.assertEquals(18,toBeTest.add(8,9));
     }
 
@@ -35,13 +26,11 @@ public class ToBeTestTest {
 
     @Test(expected = MathException.class)
     public void mod() {
-        IToBeTest toBeTest = new ToBeTest();
         toBeTest.mod(1,0);
     }
 
     @Test
     public void rm(){
-        IToBeTest toBeTest = new ToBeTest();
 //        Assert.assertEquals(new ResponseMessage(0,"成功"),toBeTest.rm());
         Assert.assertNull("abc",toBeTest.rm());
     }
